34e9cf467cf6e27b3b76eb967ac1c32981db3d5c
Jerom Sanjeevan JFR <jerom@ecsolutionsltd.co.uk>
2025-07-29 20:17:56 +0100

    Updated About Us page and layout structure

--- src/app/CirtyLighthouseChurch/home-lighthouse-church/home-lighthouse-church.ts
+++ src/app/CirtyLighthouseChurch/home-lighthouse-church/home-lighthouse-church.ts
@@ -8,21 +8,26 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrl: './home-lighthouse-church.css'
 })
 export class HomeLighthouseChurch {
- videoUrls: SafeResourceUrl[] = [];
-  selectedVideoUrl: SafeResourceUrl | null = null;
+isLoading = true;  // show spinner initially
+
+  videoUrls: SafeResourceUrl[] = [];
 
   constructor(private sanitizer: DomSanitizer) {
-    const urls = [
-      'https://www.youtube.com/embed/Sn9CSxQsyWI',
-      'https://www.youtube.com/embed/PPo1HxlMqIw',
-      'https://www.youtube.com/embed/Tmsx-Boa0jc',
-      'https://www.youtube.com/embed/Xxfej2ullGA',
-    ];
+    // Simulate data loading
+    setTimeout(() => {
+      const urls = [
+        'https://www.youtube.com/embed/Sn9CSxQsyWI',
+        'https://www.youtube.com/embed/PPo1HxlMqIw',
+        'https://www.youtube.com/embed/Tmsx-Boa0jc',
+        'https://www.youtube.com/embed/Xxfej2ullGA',
+      ];
 
-    this.videoUrls = urls.map((url) =>
-      this.sanitizer.bypassSecurityTrustResourceUrl(url)
-    );
+      this.videoUrls = urls.map(url =>
+        this.sanitizer.bypassSecurityTrustResourceUrl(url)
+      );
+
+      this.isLoading = false;  // hide spinner after loading
+    }, 900); // simulate 1.5 seconds loading delay
   }
-
 
 }
